09e48b5ef4699ffeac8fe1df1a88a80171fde615
Rodrigo Lopez <rodrigoalt15@gmail.com>
2022-11-05 14:25:11 -0500

    Creación perfil de Artista
    Creación perfil de Artista, falta poner header y conectar botones.

--- components/footer_nav.js
+++ components/footer_nav.js
@@ -6,7 +6,7 @@ class FooterNav extends HTMLElement {
                 <a href="mobile_app/home"><img src="../assets/home.svg" alt="home"></a>
                 <a href="mobile_app/shop"><img src="../assets/shop.svg" alt="shop"></a>
                 <a href="mobile_app/chat"><img src="../assets/chat.svg" alt="chat"></a>
-                <a href="mobile_app/profile"><img src="../assets/profile.svg" alt="profile"></a>
+                <a href="../in_app/perfil-cliente.html"><img src="../assets/profile.svg" alt="profile"></a>
             </nav>
         </div>`
     }
