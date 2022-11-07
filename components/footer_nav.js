273df5776e37320fdfd6b4281afcc285dbc72606
Rodrigo Lopez <rodrigoalt15@gmail.com>
2022-11-06 22:29:57 -0500

    Redireccion a pagina
    Se arreglo la direccion a perfil de artista

--- components/footer_nav.js
+++ components/footer_nav.js
@@ -6,7 +6,7 @@ class FooterNav extends HTMLElement {
                 <a href="../inicio/inicio.html"><img src="../assets/img/home.svg" alt="home"></a>
                 <a href="mobile_app/shop"><img src="../assets/img/shop.svg" alt="search"></a>
                 <a href="mobile_app/chat"><img src="../assets/img/chat.svg" alt="search"></a>
-                <a href="mobile_app/profile"><img src="../assets/img/profile.svg" alt="search"></a>
+                <a href="../in_app/perfil-cliente.html"><img src="../assets/img/profile.svg" alt="search"></a>
             </nav>
         </div>`
     }
