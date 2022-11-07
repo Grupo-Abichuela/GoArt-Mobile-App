6f208cac895de2a04780611197315effcb5e29d0
CATorresP <102825776+CATorresP@users.noreply.github.com>
2022-11-07 10:36:43 -0500

    cesar: correcion de error en commit
    modificacion de footer nav

--- components/footer_nav.js
+++ components/footer_nav.js
@@ -3,11 +3,6 @@ class FooterNav extends HTMLElement {
         this.innerHTML =`
         <div class="app-footer-nav">
             <nav class="footer-nav-buttons">
-
-                <a href="mobile_app/home"><img src="../assets/home.svg" alt="home"></a>
-                <a href="mobile_app/shop"><img src="../assets/shop.svg" alt="search"></a>
-                <a href="mobile_app/chat"><img src="../assets/chat.svg" alt="search"></a>
-                <a href="mobile_app/profile"><img src="../assets/profile.svg" alt="search"></a>
                 <a href="../inicio/inicio.html"><img src="../assets/img/home.svg" alt="home"></a>
                 <a href="mobile_app/shop"><img src="../assets/img/shop.svg" alt="search"></a>
                 <a href="mobile_app/chat"><img src="../assets/img/chat.svg" alt="search"></a>
