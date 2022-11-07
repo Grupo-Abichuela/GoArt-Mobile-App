a22929e23b8f31d4e1fa64eb942f5f7ce99e59db
Jose Santisteban <josesantisteban062@gmail.com>
2022-11-06 22:59:15 -0500

    errores css mejorados

---
+++ components/enviar_reseña.js
@@ -0,0 +1,18 @@
+
+
+const paragraph = document.getElementById("edit");
+const edit_button = document.getElementById("edit-button");
+const end_button = document.getElementById("end-editing");
+
+
+edit_button.addEventListener("click", function() {
+    paragraph.contentEditable = true;
+    paragraph.style.backgroundColor = "#dddbdb";
+    end_button.style.visibility = "visible";
+  } );
+
+end_button.addEventListener("click", function() {
+    paragraph.contentEditable = false;
+    paragraph.style.backgroundColor = "transparent";
+    end_button.style.visibility = "hidden";
+  } )
